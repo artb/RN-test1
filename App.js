12a55b62bd9e5951fbe9f14636e40720ce1a7d53
artbinda <artbinda@gmail.com>
2020-06-21 14:36:59 -0400

    Add: remove item from list

--- App.js
+++ App.js
@@ -13,6 +13,12 @@ export default function App() {
     setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: goalDescription}]);
   }
 
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  }
+
   return (
     <View style={styles.container}>
     <GoalInput onAddGoal={addGoalHandler} />
@@ -20,7 +26,7 @@ export default function App() {
         keyExtractor={(item, index) => item.id} 
         data={courseGoals} 
         renderItem={itemData => 
-          <GoalItem description={itemData.item.value} />} 
+          <GoalItem onDelete={() => removeGoalHandler(itemData.item.id)} description={itemData.item.value} />} 
       />
     </View>
   );
